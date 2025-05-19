080e5fc978cb76f1e83746feffc3b4e9b96c431c
Mosspiy <165090848+Mosspiy@users.noreply.github.com>
2025-05-19 20:51:16 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,15 +1,26 @@
-
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres'
-  },
-  test: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres'
+    username: 'ecommerce_6fno_user',
+    password: 'CAzMSHfV7BIvQoBfFgh2ThE0ry0R70tC',
+    database: 'ecommerce_6fno',
+    host: 'dpg-d0lihd1r0fns738ducig-a',
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      }
+    }
   },
   production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   }
 };
